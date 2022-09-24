d7e521a82d091c115638925403a187e0e2c03094
jihwooon <jihwooon@gmail.com>
2022-09-25 00:16:46 +0900

    단일 책임 원칙

---
+++ app/src/main/java/software/ch2/BankStatementCSVParser.java
@@ -0,0 +1,58 @@
+package software.ch2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BankStatementCSVParser {
+    private static final String RESOURCE = "/Users/jihwooon/Documents/Real-World-Software-Development/app/src/main/resources/";
+    private static final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    private BankTransaction parseFromCSV(final String line) {
+        final String[] columns = line.split(",");
+
+        final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+        final double amount = Double.parseDouble(columns[1]);
+        final String description = columns[2];
+
+        return new BankTransaction(date, amount, description);
+    }
+
+    public List<BankTransaction> parseLinesFromCSV(final List<String> linses) {
+        ArrayList<BankTransaction> bankTransactions = new ArrayList<>();
+        for (final String line : linses) {
+            bankTransactions.add(parseFromCSV(line));
+        }
+        return bankTransactions;
+    }
+
+    private static double calculateTotalAmount(final List<BankTransaction> bankTransactions) {
+        return bankTransactions.stream().mapToDouble(BankTransaction::getAmount).sum();
+    }
+
+    private static List<BankTransaction> selectInMonth(final List<BankTransaction> bankTransactions, final Month month) {
+        return bankTransactions.stream()
+                .filter(bankStatement -> month.equals(bankStatement.getDate().getMonth()))
+                .collect(Collectors.toList());
+    }
+
+    public static void main(final String[] args) throws IOException {
+        BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser();
+
+        final Path path = Paths.get(RESOURCE + "bank-data-simple.csv");
+        final List<String> lines = Files.readAllLines(path);
+
+        final List<BankTransaction> bankTransactions = bankStatementCSVParser.parseLinesFromCSV(lines);
+
+        System.out.println("The total for all transactions is " + calculateTotalAmount(bankTransactions));
+        System.out.println("Transactions in january " + selectInMonth(bankTransactions, Month.JANUARY));
+    }
+
+}
---
+++ app/src/main/java/software/ch2/BankTransactionAnalyzerDateTime.java
@@ -0,0 +1,31 @@
+package software.ch2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class BankTransactionAnalyzerDateTime {
+    private static final String RESOURCE = "/Users/jihwooon/Documents/Real-World-Software-Development/app/src/main/resources/";
+
+    public static void main(String[] args) throws IOException {
+        final Path path = Paths.get(RESOURCE + "bank-data-simple.csv");
+        final List<String> lines = Files.readAllLines(path);
+        double total = 0d;
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        for (String line : lines) {
+            final String[] columns = line.split(",");
+            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+            if (date.getMonth() == Month.JANUARY) {
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
+        }
+
+        System.out.println("total = " + total);
+    }
+}
