47cac0e187d1c3160e9da09df83861300eb03f62
jihwooon <jihwooon@gmail.com>
2022-09-24 21:40:21 +0900

    first init

---
+++ app/src/main/java/software/ch2/BankTransactionAnalyzerSimple.java
@@ -0,0 +1,24 @@
+package software.ch2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class BankTransactionAnalyzerSimple {
+    private static final String RESOURCE = "/Users/jihwooon/Documents/Real-World-Software-Development/app/src/main/resources/";
+
+    public static void main(String[] args) throws IOException {
+        final Path path = Paths.get(RESOURCE + "bank-data-simple.csv");
+        final List<String> lines = Files.readAllLines(path);
+        double total = 0d;
+        for (String line : lines) {
+            final String[] columns = line.split(",");
+            final double amount = Double.parseDouble(columns[1]);
+            total += amount;
+        }
+
+        System.out.println("total = " + total);
+    }
+}
